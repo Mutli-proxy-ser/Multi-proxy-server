12513da6d462b79ad2a17f96034de0cd028865ec
brightodedo <54002497+brightodedo@users.noreply.github.com>
2023-10-12 23:56:37 -0400

    files needed to begin project

---
+++ ProxyClient.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class ProxyClient {
+    public static void main(String[] args) {
+
+        // Proxy server address
+        String proxyHost = "localhost";
+
+        // Proxy server port
+        int proxyPort = 8888;
+
+        // Get the user's website
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What website do you want to visit (include https): ");
+        String targetURL = scanner.nextLine(); // Target URL to access through the proxy
+
+        try {
+            Socket proxySocket = new Socket(proxyHost, proxyPort);
+            PrintWriter proxyOut = new PrintWriter(proxySocket.getOutputStream());
+
+            // Send an HTTP GET request to the proxy server
+            proxyOut.println("GET " + targetURL + " HTTP/1.1");
+            proxyOut.println("Host: " + targetURL);
+            proxyOut.println();
+            proxyOut.flush();
+            // Read and print the response from the proxy server
+            BufferedReader proxyIn = new BufferedReader(new InputStreamReader(proxySocket.getInputStream()));
+            String responseLine;
+            while ((responseLine = proxyIn.readLine()) != null) {
+                System.out.println(responseLine);
+            }
+
+            // Close the proxy socket
+            proxySocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ProxyServer.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.net.*;
+import java.util.Arrays;
+import java.util.concurrent.*;
+
+public class ProxyServer {
+    public static void main(String[] args) {
+        int port = 8888; // Port on which the proxy server listens
+        ServerSocket serverSocket;
+
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Proxy server is listening on port " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ProxyThread proxyThread = new ProxyThread(clientSocket);
+                Thread thread = new Thread(proxyThread);
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ProxyThread implements Runnable {
+    private Socket clientSocket;
+
+    public ProxyThread(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Read client request
+            BufferedReader clientIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String requestLine = clientIn.readLine();
+            if (requestLine == null) {
+                clientSocket.close();
+                return;
+            }
+
+            // Extract target server and port from the request
+            String[] parts = requestLine.split(" ");
+            String targetHost = parts[1];
+            int targetPort = 80; // Default HTTP port
+
+            // Create a socket to connect to the target server
+            //Socket targetSocket = new Socket(targetHost.strip(), targetPort); // statement causing the error
+            System.out.println("Successfully connected to client server");
+
+            //Create a client writer to output the information to the client
+            PrintWriter clientOut = new PrintWriter(clientSocket.getOutputStream());
+            String responseLine;
+
+            // connect to the website for the user and return the response to the user
+            URL ur = new URL(targetHost);
+            URLConnection conn = ur.openConnection();
+
+            // create an inputstreamreader stream to get information from the target server
+            BufferedReader serverIn = new BufferedReader( new InputStreamReader(conn.getInputStream()));
+
+            while ((responseLine = serverIn.readLine()) != null) {
+                clientOut.println(responseLine);
+            }
+            clientOut.flush();
+
+            // Close sockets
+            clientSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
