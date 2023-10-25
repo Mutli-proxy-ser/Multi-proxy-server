e810595272fb8eb50b25ea6aeee056f7bfb11125
Jawuah1 <jessekofia@hotmail.com>
2023-10-25 15:58:59 -0400

    updated client file

--- ProxyClient.java
+++ ProxyClient.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class ProxyClient {
     public static void main(String[] args) {
@@ -15,13 +18,16 @@ public class ProxyClient {
 
         // Proxy server port
         int proxyPort = 8888;
-
+        // add extra comment
         // Get the user's website
         Scanner scanner = new Scanner(System.in);
         System.out.print("What website do you want to visit (include https): ");
         String targetURL = scanner.nextLine(); // Target URL to access through the proxy
 
         try {
+            URI uri = new URI(targetURL);
+            Desktop.getDesktop().browse(uri);
+
             Socket proxySocket = new Socket(proxyHost, proxyPort);
             PrintWriter proxyOut = new PrintWriter(proxySocket.getOutputStream());
 
@@ -39,7 +45,7 @@ public class ProxyClient {
 
             // Close the proxy socket
             proxySocket.close();
-        } catch (IOException e) {
+        } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
         }
     }
